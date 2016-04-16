a09cbf599647cdf935fe455ecbebbf4a579fea2b
Yakka <emmanuel.corno@neuf.fr>
2016-04-16 20:08:12 +0200

    Mouse button raycasting.

---
+++ Hausmann/Assets/Scripts/BuildingManipulator.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class BuildingManipulator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                Debug.Log(hit.transform.gameObject.name);
+            }
+        }
+	}
+}
