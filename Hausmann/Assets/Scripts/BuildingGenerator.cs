75b1563fa5996c62a1958064475f8815fa950dce
Yakka <emmanuel.corno@neuf.fr>
2016-04-16 12:18:01 +0200

    First window.

---
+++ Hausmann/Assets/Scripts/BuildingGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BuildingGenerator : MonoBehaviour {
+    
+    public List<Piece> pieces = new List<Piece>();
+
+    public int numberOfLevels;
+    public int numberOfRows;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Hausmann/Assets/Scripts/Piece.cs
+++ Hausmann/Assets/Scripts/Piece.cs
@@ -4,23 +4,19 @@ using System.Collections.Generic;
 
 // A piece of a building.
 // Pieces obey to constraints.
-public class Piece {
+public class Piece : MonoBehaviour {
 
     public enum Constraint {
         None = 0,
+        Bottom,
+        Top,
         NumberOfConstraints
     }
 
     public List<Constraint> constraints = new List<Constraint>();
-
+    public int level;
+    public int column;
     public GameObject plane; // TODO: the displayed plane
-
-    public Piece(List<Constraint> _constraints) {
-        foreach(Constraint constraint in _constraints) {
-            constraints.Add(constraint);
-        }
-
-    }
 
     // Checks if a list of constraints match with this object's constraints
     public bool IsMatching(List<Constraint> _constraintsToCompare) {
@@ -42,7 +38,31 @@ public class Piece {
 
     // Checks if two constraints match together
     private bool IsMatching(Constraint _constraint1, Constraint _constraint2) {
-        return true; // TODO
+        bool isMatching = true;
+        // Switch of hell
+        switch(_constraint1) {
+            // Bottom:
+            case Constraint.Bottom:
+                switch(_constraint2) {
+                    case Constraint.Bottom:
+                        isMatching = false;
+                        break;
+                }
+                break;
+
+            // Top:
+            case Constraint.Top:
+                switch (_constraint2) {
+                    case Constraint.Top:
+                        isMatching = false;
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+
+        return isMatching;
     }
 
 
