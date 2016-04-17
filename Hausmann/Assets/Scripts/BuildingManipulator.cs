3eb8077517c815bfdffdc8280e42d6147dc1a248
Yakka <emmanuel.corno@neuf.fr>
2016-04-17 14:28:16 +0200

    Added cursors.

--- Hausmann/Assets/Scripts/BuildingManipulator.cs
+++ Hausmann/Assets/Scripts/BuildingManipulator.cs
@@ -2,18 +2,28 @@ using UnityEngine;
 using System.Collections;
 
 public class BuildingManipulator : MonoBehaviour {
+    [System.Serializable]
+    public struct CursorTextures {
+        public Texture2D release;
+        public Texture2D holden;
+    }
 
     private bool holdingRoof = false;
     private Building grabbedBuilding;
     private int roofLevel;
     private Vector3 lastMousePosition;
-	
-	void Update () {
-        if (Input.GetMouseButton(0)) {
+
+    public CursorTextures standardCursor;
+    public CursorTextures dragDropCursor;
+
+    void Update () {
+        // Click
+        if (Input.GetMouseButton(0)) { // TODO: refaire avec une fonction qui d�tecte � quel �tage est la souris (objets invisibles qui raycast?)
             Piece piece = RaycastMouseToPiece();
             if (holdingRoof) { // Test if the player has moved the mouse enough
+                Cursor.SetCursor(dragDropCursor.holden, Vector2.zero, CursorMode.ForceSoftware);
                 // Moving down:
-                if(piece != null && piece.level > 0 && piece.level < roofLevel) {
+                if (piece != null && piece.level > 0 && piece.level < roofLevel) {
                     grabbedBuilding.MoveRoofToLevel(piece.level);
                     lastMousePosition = Input.mousePosition;
                     roofLevel--;
@@ -25,7 +35,8 @@ public class BuildingManipulator : MonoBehaviour {
                     lastMousePosition = Input.mousePosition;
                 }
             } else { // Test if the player is on the roof:
-                if(piece != null && piece.isRoof) {
+                Cursor.SetCursor(standardCursor.holden, Vector2.zero, CursorMode.ForceSoftware);
+                if (piece != null && piece.isRoof) {
                     holdingRoof = true;
                     roofLevel = piece.level;
                     grabbedBuilding = piece.GetComponentInParent<Building>();
@@ -38,6 +49,11 @@ public class BuildingManipulator : MonoBehaviour {
             
         } else {
             holdingRoof = false;
+            if(RaycastMouseToRoof() != null) { // Drag & drop cursor:
+                Cursor.SetCursor(dragDropCursor.release, Vector2.zero, CursorMode.ForceSoftware);
+            } else { // Standard cursor:
+                Cursor.SetCursor(standardCursor.release, Vector2.zero, CursorMode.ForceSoftware);
+            }
         }
 	}
 
@@ -47,6 +63,14 @@ public class BuildingManipulator : MonoBehaviour {
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit)) {
             piece = hit.transform.gameObject.GetComponent<Piece>();
+        }
+        return piece;
+    }
+
+    private Piece RaycastMouseToRoof() {
+        Piece piece = RaycastMouseToPiece();
+        if (piece != null && !piece.isRoof) {
+            piece = null;
         }
         return piece;
     }
